a8a88b91e715e2d8dc635364517ec60b7cacd7ec
Abioye Bankole <bioyexx@yahoo.com>
2019-06-12 18:07:00 +0100

    Created PollingUnit.java, updated applications.properties to generate polling_unit table

--- src/main/java/com/naijagov/naijagov/NaijagovApplication.java
+++ src/main/java/com/naijagov/naijagov/NaijagovApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//(scanBasePackages = {"com.naijagov.naijagov.model" })
 public class NaijagovApplication {
 
 	public static void main(String[] args) {
