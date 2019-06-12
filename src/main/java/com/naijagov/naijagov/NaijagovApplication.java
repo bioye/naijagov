17591ed0f6f08a64e8c8bcfd03ec6cb5567aaae5
Abioye Bankole <bioyexx@yahoo.com>
2019-06-12 23:06:44 +0100

    index, hal and raw endpoints available on the url

--- src/main/java/com/naijagov/naijagov/NaijagovApplication.java
+++ src/main/java/com/naijagov/naijagov/NaijagovApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//(scanBasePackages = {"com.naijagov.naijagov.model" })
 public class NaijagovApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/naijagov/naijagov/repository/PollingUnitRepository.java
+++ src/main/java/com/naijagov/naijagov/repository/PollingUnitRepository.java
@@ -3,8 +3,18 @@ package com.naijagov.naijagov.repository;
 import com.naijagov.naijagov.model.PollingUnit;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
-public interface PollingUnitRepository extends CrudRepository<PollingUnit, Long>{
+@Repository
+public interface PollingUnitRepository extends CrudRepository<PollingUnit, Integer>{
+    // Prevents POST /pollingUnit and PATCH /pollingUnit/:id
+    @Override
+    @RestResource(exported = false)
+    public PollingUnit save(PollingUnit p);
+
+    // Prevents DELETE /pollingUnit/:id
+    @Override
+    @RestResource(exported = false)
+    public void delete(PollingUnit p);
 }
