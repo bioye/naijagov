282fe21ca220b791c3cf010a6d5a130792d40b8e
Abioye Bankole <bioyexx@yahoo.com>
2019-06-23 04:55:08 +0100

    introduced simple dynamic paging and ability to change page size dynamically to pollingunits page react version

--- src/main/resources/static/js/pollingunits.js
+++ src/main/resources/static/js/pollingunits.js
@@ -4,47 +4,160 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const client = require('./client');
 
+const follow = require('./follow');
+
+const root = '/api';
+
 class PollingUnitsComponent extends React.Component {
 
 	constructor(props) {
 		super(props);
-		this.state = {pollingunits: []};
+		this.state = {pollingunits: [], attributes: [], pageSize: 10, links: {}};
+		this.updatePageSize = this.updatePageSize.bind(this);
+		this.onNavigate = this.onNavigate.bind(this);
 	}
-	componentDidMount() {
-		client({method: 'GET', path: '/api/pollingUnits'}).done(response => {
-			this.setState({pollingunits: response.entity._embedded.pollingUnits});
+
+	loadFromServer(pageSize) {
+		follow(client, root, [
+			{rel: 'pollingUnits', params: {size: pageSize}}]
+		).then(pollingunitCollection => {
+			return client({
+				method: 'GET',
+				path: pollingunitCollection.entity._links.profile.href,
+				headers: {'Accept': 'application/schema+json'}
+			}).then(schema => {
+				this.schema = schema.entity;
+				return pollingunitCollection;
+			});
+		}).done(pollingunitCollection => {
+			this.setState({
+				pollingunits: pollingunitCollection.entity._embedded.pollingUnits,
+				attributes: Object.keys(this.schema.properties),
+				pageSize: pageSize,
+				links: pollingunitCollection.entity._links});
 		});
+	}
+
+	onNavigate(navUri) {
+		client({method: 'GET', path: navUri}).done(pollingunitCollection => {
+			this.setState({
+				pollingunits: pollingunitCollection.entity._embedded.pollingUnits,
+				attributes: this.state.attributes,
+				pageSize: this.state.pageSize,
+				links: pollingunitCollection.entity._links
+			});
+		});
+	}
+
+	updatePageSize(pageSize) {
+		if (pageSize !== this.state.pageSize) {
+			this.loadFromServer(pageSize);
+		}
+	}
+
+	componentDidMount() {
+		this.loadFromServer(this.state.pageSize);
 	}
 	render() {
 		return (
-			<PollingUnits pollingunits={this.state.pollingunits}/>
+			<div>
+				<PollingUnits pollingunits={this.state.pollingunits}
+							  links={this.state.links}
+							  pageSize={this.state.pageSize}
+							  onNavigate={this.onNavigate}
+							  updatePageSize={this.updatePageSize}/>
+			</div>
 		)
 	}
 }
+
 class PollingUnits extends React.Component{
+
+	constructor(props) {
+		super(props);
+		this.handleNavFirst = this.handleNavFirst.bind(this);
+		this.handleNavPrev = this.handleNavPrev.bind(this);
+		this.handleNavNext = this.handleNavNext.bind(this);
+		this.handleNavLast = this.handleNavLast.bind(this);
+		this.handleInput = this.handleInput.bind(this);
+	}
+
+	handleInput(e) {
+		e.preventDefault();
+		const pageSize = ReactDOM.findDOMNode(this.refs.pageSize).value;
+		if (/^[0-9]+$/.test(pageSize)) {
+			this.props.updatePageSize(pageSize);
+		} else {
+			ReactDOM.findDOMNode(this.refs.pageSize).value =
+				pageSize.substring(0, pageSize.length - 1);
+		}
+	}
+	
+	handleNavFirst(e){
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.first.href);
+	}
+
+	handleNavPrev(e) {
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.prev.href);
+	}
+
+	handleNavNext(e) {
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.next.href);
+	}
+
+	handleNavLast(e) {
+		e.preventDefault();
+		this.props.onNavigate(this.props.links.last.href);
+	}
+	
 	render() {
-		//if (this.props.data) {
-			const pollingunits = this.props.pollingunits.map(pollingunit =>
-				<PollingUnit key={pollingunit._links.self.href} pollingunit={pollingunit}/>
-			);
-		//}
+		const pollingunits = this.props.pollingunits.map(pollingunit =>
+			<PollingUnit key={pollingunit._links.self.href} pollingunit={pollingunit}/>
+		);
+		const navLinks = [];
+		if ("first" in this.props.links) {
+			navLinks.push(<button key="first" onClick={this.handleNavFirst}>&lt;&lt;</button>);
+		}
+		if ("prev" in this.props.links) {
+			navLinks.push(<button key="prev" onClick={this.handleNavPrev}>&lt;</button>);
+		}
+		if ("next" in this.props.links) {
+			navLinks.push(<button key="next" onClick={this.handleNavNext}>&gt;</button>);
+		}
+		if ("last" in this.props.links) {
+			navLinks.push(<button key="last" onClick={this.handleNavLast}>&gt;&gt;</button>);
+		}
 		return (
-			<table>
-				<tbody>
-					<tr>
-                        <th>#</th>
-                        <th>Code</th>
-                        <th>Description</th>
-                        <th>Longitude</th>
-                        <th>Latitude</th>
-					</tr>
-						{pollingunits}
-				</tbody>
-			</table>
+			<div>
+				<input ref="pageSize" defaultValue={this.props.pageSize} onInput={this.handleInput}/>
+				<table>
+					<tbody>
+						<tr>
+							<th>#</th>
+							<th>Code</th>
+							<th>Description</th>
+							<th>Longitude</th>
+							<th>Latitude</th>
+						</tr>
+							{pollingunits}
+					</tbody>
+				</table>
+				<div>
+					{navLinks}
+				</div>
+			</div>
 		)
 	}
 }
 class PollingUnit extends React.Component{
+
+	constructor(props) {
+		super(props);
+	}
+
 	render() {
 		return (
 			<tr>
