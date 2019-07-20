4a43153188815f2abfbae844eb94d375fda6e6a2
Abioye Bankole <bioyexx@yahoo.com>
2019-07-20 06:57:51 +0100

    Reformatted Form.java and moved fields upwards as per Codacy suggestions.

--- src/main/java/com/naijagov/naijagov/model/Form.java
+++ src/main/java/com/naijagov/naijagov/model/Form.java
@@ -4,49 +4,49 @@ import org.springframework.data.domain.Page;
 
 public class Form {
 
+  private Page<Ward> wardsPage;
+  private Page<PollingUnit> wardPage;
+  private Iterable<Ward> wards;
+  private Page<PollingUnit> pollingUnitsPage;
+  private Iterable<PollingUnit> pollingUnits;
+
   public Page<PollingUnit> getWardPage() {
-      return wardPage;
+    return wardPage;
   }
 
-    public Page<Ward> getWardsPage() {
-        return wardsPage;
-    }
+  public Page<Ward> getWardsPage() {
+    return wardsPage;
+  }
 
-    public Page<PollingUnit> getPollingUnitsPage() {
-        return pollingUnitsPage;
-    }
+  public Page<PollingUnit> getPollingUnitsPage() {
+    return pollingUnitsPage;
+  }
 
-    public Iterable<Ward> getWards() {
-        return wards;
-    }
+  public Iterable<Ward> getWards() {
+    return wards;
+  }
 
-    public Iterable<PollingUnit> getPollingUnits() {
-        return pollingUnits;
-    }
+  public Iterable<PollingUnit> getPollingUnits() {
+    return pollingUnits;
+  }
 
-    public void setWardPage(Page<PollingUnit> wardPage) {
-        this.wardPage = wardPage;
-    }
+  public void setWardPage(Page<PollingUnit> wardPage) {
+    this.wardPage = wardPage;
+  }
 
-    public void setWardsPage(Page<Ward> wardsPage) {
-        this.wardsPage = wardsPage;
-    }
+  public void setWardsPage(Page<Ward> wardsPage) {
+    this.wardsPage = wardsPage;
+  }
 
-    public void setPollingUnitsPage(Page<PollingUnit> pollingUnitPage) {
-        this.pollingUnitsPage = pollingUnitPage;
-    }
+  public void setPollingUnitsPage(Page<PollingUnit> pollingUnitPage) {
+    this.pollingUnitsPage = pollingUnitPage;
+  }
 
-    public void setWards(Iterable<Ward> wards) {
-        this.wards = wards;
-    }
+  public void setWards(Iterable<Ward> wards) {
+    this.wards = wards;
+  }
 
-    public void setPollingUnits(Iterable<PollingUnit> pollingUnits) {
-        this.pollingUnits = pollingUnits;
-    }
-
-    private Page<Ward> wardsPage;
-    private Page<PollingUnit> wardPage;
-    private Iterable<Ward> wards;
-    private Page<PollingUnit> pollingUnitsPage;
-    private Iterable<PollingUnit> pollingUnits;
+  public void setPollingUnits(Iterable<PollingUnit> pollingUnits) {
+    this.pollingUnits = pollingUnits;
+  }
 }
