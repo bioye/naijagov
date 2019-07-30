0bcd70fa05cbd4415932f5a2e40a6ecac513b50e
Abioye Bankole <bioyexx@yahoo.com>
2019-07-30 23:52:26 +0100

    Updated LocalGovService members

--- src/main/java/com/naijagov/naijagov/service/LocalGovService.java
+++ src/main/java/com/naijagov/naijagov/service/LocalGovService.java
@@ -1,5 +1,31 @@
 package com.naijagov.naijagov.service;
 
+import java.util.Optional;
+
+import com.naijagov.naijagov.model.LocalGov;
+import com.naijagov.naijagov.repository.LocalGovRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+@Service("localGovService")
 public class LocalGovService{
+  private LocalGovRepository localGovRepository;
+
+  public Optional<LocalGov> findLocalGov(int id) {
+    return localGovRepository.findById(id);
+  }
+
+  public Page<LocalGov> listAlllocalGovs(Pageable page) {
+    return localGovRepository.findAll(page);
+  }
+
+  @Autowired
+  public LocalGovService(LocalGovRepository localGovRepository) {
+    this.localGovRepository = localGovRepository;
+  }
+  
 
 }
